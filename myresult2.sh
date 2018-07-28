89eab648aecf8c8a3f878ffd3716f622de25e477
John Szabo <john.szabo.jr@gmail.com>
2018-07-28 19:03:41 +0200

    rm

--- myresult2.sh
+++
@@ -1,2 +0,0 @@
-ARG="Backward" && ../../../../../../Downloads/vm_champs/asm ../../../../../\
-../Downloads/vm_champs/champs/$ARG.s && ./asm ../../../../../../Downloads/vm_champs/champs/$ARG.s | rev | cut -c 2- | rev > ./results/$ARG.myresult && hexdump -C ../../../../../../Downloads/vm_champs/champs/$ARG.cor | cut -c 11- | rev | cut -c 20- | rev | sed -e '$d' > ./results/$ARG.hisresult && echo '\nchecking differences...\n' && diff ./results/$ARG.hisresult ./results/$ARG.myresult && echo 'done\n'
